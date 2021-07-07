950286c1fe5b329ea0ae85451bb7242713295f68
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-07-07 20:10:16 -0300

    first commit

---
+++ backend/src/test/java/com/github/com/marcelomachadoxd/dslearn/DsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.com.marcelomachadoxd.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
