e98ff756cf852088625c9df88bf3f7fc3d83c0f3
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-08-01 20:18:58 -0300

    Exceptions

---
+++ backend/src/main/java/com/github/com/marcelomachadoxd/dslearn/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.github.com.marcelomachadoxd.dslearn.services.exceptions;
+
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
---
+++ backend/src/main/java/com/github/com/marcelomachadoxd/dslearn/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.github.com.marcelomachadoxd.dslearn.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
