a5268f252fb3ece3f673d5c020b6df37183cbcba
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-08-01 18:44:59 -0300

    Topic

--- backend/src/main/java/com/github/com/marcelomachadoxd/dslearn/entities/Lesson.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/dslearn/entities/Lesson.java
@@ -33,6 +33,10 @@ public abstract class Lesson implements Serializable {
     @OneToMany(mappedBy = "lesson")
     private List<Deliver> deliveries = new ArrayList<>();
 
+
+    @OneToMany(mappedBy = "lesson")
+    private List<Topic> topics = new ArrayList<>();
+
     public Lesson() {
     }
 
