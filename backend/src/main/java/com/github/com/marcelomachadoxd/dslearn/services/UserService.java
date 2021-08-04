98314bb1ae42cb69b1cdb1eaf174bc033798309f
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-08-04 20:55:36 -0300

    RestController UserByID

--- backend/src/main/java/com/github/com/marcelomachadoxd/dslearn/services/UserService.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/dslearn/services/UserService.java
@@ -1,7 +1,9 @@
 package com.github.com.marcelomachadoxd.dslearn.services;
 
+import com.github.com.marcelomachadoxd.dslearn.DTO.UserDTO;
 import com.github.com.marcelomachadoxd.dslearn.entities.User;
 import com.github.com.marcelomachadoxd.dslearn.entities.repositories.UserRepository;
+import com.github.com.marcelomachadoxd.dslearn.services.exceptions.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,6 +23,20 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        try {
+            Optional<User> obj = userRepository.findById(id);
+            User user = obj.orElseThrow(() -> new EntityNotFoundException("User not found with id " + id));
+
+            return new UserDTO(user);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Not found id: " + id);
+
+        }
+    }
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
